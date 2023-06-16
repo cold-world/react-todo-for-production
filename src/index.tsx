8aca624470e585fb2bc08fa6dde0071935ae04a0
Igor Andreev <systemovverload@gmail.com>
2023-06-16 12:32:34 +0300

    add ci config

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 
 import { App } from './App';
+
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 const root = ReactDOM.createRoot(document.getElementById('root') as Element);
